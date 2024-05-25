e4b4e0d98f3d2102b47a04709c08eb45d8cb6e46
acarlseen <110647552+acarlseen@users.noreply.github.com>
2024-05-25 09:33:20 -0700

    initial commit

---
+++ nav.js
@@ -0,0 +1,19 @@
+addEventListener('DOMContentLoaded', () => {
+    document.getElementsByTagName('nav')[0].innerHTML = '\
+    <div class="navbar main-bar">\
+            <div class="navbar brand">\
+                <a class="brand" href="/">Alex Carlseen</a>\
+            </div>\
+            <div class="navbar menu">\
+                <ul class="nav-list">\
+                    <li class="navbar menu-item">\
+                        <a class="menu-item" href="/">About Me</a>\
+                    </li>\
+                    <li class="navbar menu-item">\
+                        <a class="menu-item" href="/contact.html">Contact</a>\
+                    </li>\
+                </ul>\
+            </div>\
+        </div>\
+        <div class="spacer"></div>'
+})
